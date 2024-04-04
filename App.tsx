46f51f944cb6c9522c164e1e75b888c7542a43a6
RuanDevJs <ruanelpidio@hotmail.com>
2024-04-03 22:25:32 -0300

    Finished Project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View, Button, TouchableOpacity, Alert } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import * as LocalAuthentication from "expo-local-authentication";
+
+import { styles } from "./style";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verifyAvaliableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(
+      "Tipos de autenticação: ",
+      types.map((type) => LocalAuthentication.AuthenticationType[type])
+    );
+  }
+
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    console.log("isBiometricEnrolled", isBiometricEnrolled);
+
+    if (!isBiometricEnrolled)
+      return Alert.alert("Login", "Não há biometria registrada");
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Desbloqueie seu celular",
+      fallbackLabel: "Biometria não reconhecida",
+    });
+
+    return auth ? setIsAuthenticated(true) : setIsAuthenticated(false);
+  }
+
+  useEffect(() => {
+    verifyAvaliableAuthentication();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Usuário conectado: {isAuthenticated ? "Sim" : "Não"}
+      </Text>
+      <TouchableOpacity
+        onPress={handleAuthentication}
+        style={styles.button}
+        activeOpacity={0.72}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    color: "#000",
+    marginVertical: 32,
+  },
+  button: {
+    width: 220,
+    padding: 14,
+    backgroundColor: "#84e",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  buttonText: {
+    fontSize: 12,
+    color: "#fff",
+  },
+});
